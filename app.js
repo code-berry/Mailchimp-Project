6dc7c9fa519f2822c054b9761e1ae5e2342ff127
Matt Berry <matt@codeberry.ca>
2019-02-26 19:07:13 -0500

    finish homepage layout, added styles, post request complete

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req,res) {
+	res.sendFile(__dirname + "/signup.html");
+});
+
+app.post('/', function(req,res){
+	var firstName = req.body.fName;
+	var lastName = req.body.lName;
+	var email = req.body.email;
+	var fullName = firstName + ' ' + lastName;
+	console.log(email, fullName);
+
+	var data = {
+		members: [
+			{
+			 email_address: email,
+			 status: "subscribed"
+			}
+		]
+	};
+
+	var jsonData = JSON.stringify(data);
+
+	var options = {
+		url: "https://us20.api.mailchimp.com/3.0/lists/70d27a38e9",
+		method: "POST",
+		headers: {
+			"Authorization": "matt e0820458ed60b09f8e9c22101d87ebad-us20"
+		},
+		body: jsonData
+	};
+
+	request(options, function(error, response, body) {
+		if (error) {
+			console.log("Error");
+		} else {
+			console.log(response.statusCode);
+		}
+	});
+});
+
+
+
+app.listen(3000, function() {
+	console.log('Server is running on port 3000');
+});
+
+
+
+// 70d27a38e9
+// e0820458ed60b09f8e9c22101d87ebad-us20
